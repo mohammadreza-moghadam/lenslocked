c4c6b2be2f548f70ff7eb9fe3d77679cca1af14d
mohammadreza-moghadam <mohammadreza.che.eng@gmail.com>
2022-03-31 03:12:50 -0700

    add gorilla mux router and refactor old code

--- main.go
+++ main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Go Language</h1>")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Go Language!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Tp get in touch, please send an email to <a "+
+		"href=\"mohammadreza.che.eng@gmail.com\">support@mohammadreza.com</a>")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Println("This is a FAQ Page")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find what you are looking for!</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("server is running...")
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	// 404 page
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+
+	http.ListenAndServe(":8080", r)
 }
